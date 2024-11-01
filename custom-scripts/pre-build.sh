e9e36c59a7be7f6c4ea305dedebeab31fa3725db
Vítor Aguirre Caús <118321479+VitorCaus@users.noreply.github.com>
2024-11-01 16:37:25 +0000

    tutoriais 2.2 e 2.3 refeitos; 2.4 testado; falta recolocar T2

--- custom-scripts/pre-build.sh
+++ custom-scripts/pre-build.sh
@@ -8,3 +8,6 @@ cp $BASE_DIR/../linux-4.13.9/arch/x86/configs/qemu_x86_custom_defconfig $BASE_DI
 chmod +x $BASE_DIR/target/etc/init.d/S41network-config
 chmod +x $BASE_DIR/target/etc/init.d/S50hello
 chmod +x $BASE_DIR/target/usr/bin/tp1.py
+
+# compilar modulo (tutorial 2.3)
+make -C $BASE_DIR/../modules/simple_driver/
